2180d071bea465e0213a4f9dd210788ac58fc1ff
Sumaer Jolly <sumaerjolly@gmail.com>
2019-12-20 14:33:13 +0530

    setup initial html,css and js files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert('this is working');
+import './style.css';
+const iconElement = document.querySelector('.weather-icon');
+const tempElement = document.querySelector('.temperature-value p');
+const descElement = document.querySelector('.temperature-description p');
+const locationElement = document.querySelector('.location p');
+const notificationElement = document.querySelector('.notification');
+
+const weather = {};
+
+weather.temperature = {
+  unit: 'celsius'
+};
+
+const KELVIN = 273;
+const key = '99f15a158a869cfdd635370f80c21b6b';
