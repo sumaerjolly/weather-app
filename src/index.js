fe7e211b322f7a2d79c64dce80c7ee58e15b58b6
Sumaer Jolly <sumaerjolly@gmail.com>
2019-12-20 15:11:02 +0530

    added geolocation and api working

--- src/index.js
+++ src/index.js
@@ -14,10 +14,25 @@ weather.temperature = {
 const KELVIN = 273;
 const key = '99f15a158a869cfdd635370f80c21b6b';
 
-if ('gelocation' in navigator) {
+if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
   notificationElement.style.display = 'block';
-  notificationElement.innerHTML =
-    "<p> Browser doesn't Support Geolocation </p>";
+  notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+function getWeather(latitude, longitude) {
+  let api = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  console.log(api);
+}
+
+function setPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+
+  getWeather(latitude, longitude);
+}
+
+function showError(error) {
+  notificationElement.style.display = 'block';
+  notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
